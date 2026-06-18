48a832a6909004b83993f0abaabf14d7dbaef8f2
l-shahbil <laith.715.asad@gmail.com>
2026-06-18 04:14:59 +0300

    fix contact services

--- src/features/contact/contact.controller.ts
+++ src/features/contact/contact.controller.ts
@@ -4,8 +4,8 @@ import { successResponse } from '../../shared/utils/response.js';
 import type { ContactInput } from './contact.validator.js';
 
 export const contactController = {
-  async send(req: Request, res: Response) {
-    await contactService.sendContactMessage(req.body as ContactInput);
-    successResponse(res, 'تم إرسال رسالتك بنجاح');
-  },
+ async send(req: Request, res: Response) {
+  await contactService.sendContactMessage(req.body as ContactInput);
+  res.json(successResponse(null, 'تم إرسال رسالتك بنجاح'));
+},
 };
