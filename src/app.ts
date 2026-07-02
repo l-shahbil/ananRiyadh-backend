ef1f267679055bedb1ad98920deb3d9209c2b45c
l-shahbil <laith.715.asad@gmail.com>
2026-07-02 15:06:29 +0300

    fix: check proxy temporary

--- src/app.ts
+++ src/app.ts
@@ -35,6 +35,21 @@ app.use(express.json());
 app.use('/api', generalLimiter);
 app.use('/api/auth/login', authLimiter);
 
+//temporary
+app.use((req, res, next) => {
+  const xff = req.headers['x-forwarded-for'];
+
+  console.log('X-Forwarded-For:', xff);
+  console.log('Remote Address:', req.socket.remoteAddress);
+
+  if (typeof xff === 'string') {
+    console.log('IPs:', xff.split(',').map(ip => ip.trim()));
+    console.log('Count:', xff.split(',').length);
+  }
+
+  next();
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/listings', listingsRouter);
